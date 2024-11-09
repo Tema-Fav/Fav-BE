15064154f0cc820a2ec9977e3e828f30c36d19f6
이나민 <nm0922@catholic.ac.kr>
2024-11-09 14:59:47 +0900

    [Feat] dashboard, storeInfo api 수정

--- routes/storeInfo.js
+++ routes/storeInfo.js
@@ -36,55 +36,75 @@ router.get('/search', async (req, res) => {
 });
 
 router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const storeInfo = await StoreInfo.findById(req.params.id);
+    const storeInfo = await StoreInfo.findOne({ boss_id: id }).populate(
+      'boss_id',
+    ); // boss_id로 검색
     if (!storeInfo) {
       return res.status(404).json({ error: 'StoreInfo not found' });
     }
-    res.json(storeInfo);
-  } catch (err) {
-    next(err);
+    res.status(200).json(storeInfo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
-router.post('/', async (req, res) => {
-  const { store_name, store_address, store_info, store_photo } = req.body;
+router.post('/', async (req, res, next) => {
+  const { store_name, store_address, store_info, store_photo, boss_id } =
+    req.body;
 
   try {
     // 스키마에 맞는 필드로 데이터를 생성합니다.
-    const newStoreInfo = await StoreInfo.create({
+    const newStoreInfo = new StoreInfo({
       store_name,
       store_address,
       store_info,
       store_photo,
+      boss_id,
     });
-    res.status(201).json(newStoreInfo);
+    const savedStoreInfo = await newStoreInfo.save();
+    res.status(201).json(savedStoreInfo);
   } catch (error) {
     console.error('데이터 생성 중 오류:', error);
     res.status(500).json({ error: '데이터 생성 중 오류가 발생했습니다.' });
   }
 });
 
-router.put('/:id', (req, res) => {
-  StoreInfo.findByIdAndUpdate(req.params.id, {
-    store_name: req.body.store_name,
-    store_address: req.body.store_address,
-    sotre_info: req.body.store_info,
-    store_photo: req.body.store_photo,
-  }).then((result) => {
-    res.json(result);
-  });
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { store_name, store_address, store_info, store_photo } = req.body;
+
+  try {
+    const updatedStoreInfo = await StoreInfo.findOneAndUpdate(
+      { boss_id: id },
+      { store_name, store_address, store_info, store_photo },
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedStoreInfo) {
+      return res.status(404).json({ error: 'StoreInfo not found' });
+    }
+
+    res.status(200).json(updatedStoreInfo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
   try {
-    const storeInfo = await StoreInfo.findByIdAndDelete(req.params.id);
-    if (!storeInfo) {
+    const deletedStoreInfo = await StoreInfo.findOneAndDelete({ boss_id: id });
+
+    if (!deletedStoreInfo) {
       return res.status(404).json({ error: 'StoreInfo not found' });
     }
-    res.status(204).end();
-  } catch (err) {
-    next(err);
+
+    res.status(200).json({ message: 'StoreInfo successfully deleted' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
