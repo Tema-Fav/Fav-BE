44a55ae1b0d844b25d3b6c8ae6d30a8519400a56
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-11-10 15:42:46 +0900

    refactor: 상점 등록 사진 업로드 자유화

--- models/StoreInfo.js
+++ models/StoreInfo.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const storeInfoSchema = new Schema({
+  boss_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Boss',
+    required: true,
+  },
   store_name: { type: String, required: true },
   store_address: { type: String, required: true },
   store_info: { type: String, required: true },
-  store_photo: { type: String, required: true },
+  store_photo: { type: String, required: false }, // 필수 여부 제거
   boss_id: { type: Schema.Types.ObjectId, ref: 'boss', required: true },
 });
 
