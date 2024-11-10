aab3e99dc401b3e2a61d2b47224786f30f446788
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-11-10 20:14:58 +0900

    fix: 가게 등록 api 수정

--- models/StoreInfo.js
+++ models/StoreInfo.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const storeInfoSchema = new Schema({
+const storeInfoSchema = new mongoose.Schema({
+  store_name: { type: String, required: true },
+  store_address: { type: String, required: true },
+  store_info: { type: String, required: true },
+  store_photo: { type: String, required: false },
   boss_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Boss',
     required: true,
   },
-  store_name: { type: String, required: true },
-  store_address: { type: String, required: true },
-  store_info: { type: String, required: true },
-  store_photo: { type: String, required: false }, // 필수 여부 제거
-  boss_id: { type: Schema.Types.ObjectId, ref: 'boss', required: true },
 });
 
 const StoreInfo = mongoose.model('StoreInfo', storeInfoSchema);
