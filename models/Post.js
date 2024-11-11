f1a0e72b03c8f2be3b18c77e771236ed4b1a1c75
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-11-11 14:19:12 +0900

    feat: 백엔드 수정

--- models/Post.js
+++ models/Post.js
@@ -4,37 +4,44 @@ const router = express.Router();
 const Post = require('../models/Post');
 const Boss = require('../models/Boss');
 
-const postSchema = new mongoose.Schema({
-  boss_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Boss',
-    required: true,
+const postSchema = new mongoose.Schema(
+  {
+    boss_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Boss',
+      required: true,
+    },
+    content: {
+      type: String,
+      maxlength: 600,
+    },
+    is_open: {
+      type: Boolean,
+      default: true,
+    },
+    crowd_level: {
+      type: String,
+      enum: ['LOW', 'MEDIUM', 'HIGH'],
+    },
+    store_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Store',
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    maxlength: 600,
+  {
+    timestamps: true, // created_at과 updated_at 자동 관리
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  is_open: {
-    type: Boolean,
-    default: true,
-  },
-  crowd_level: {
-    type: String,
-    enum: ['LOW', 'MEDIUM', 'HIGH'],
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
-postSchema.pre('save', function (next) {
-  this.updated_at = Date.now();
-  next();
+// Virtual field 설정
+postSchema.virtual('store', {
+  ref: 'Store',
+  localField: 'store_id',
+  foreignField: '_id',
+  justOne: true,
 });
 
 module.exports = mongoose.model('Post', postSchema);
